3de35d7394cd21ba3a264d36048966c3d6669011
sarkisovTu <sarkisov1337@gmail.com>
2022-09-19 20:44:15 +0300

    fix: react-dom render

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = document.getElementById('root');
 
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  root,
 );
