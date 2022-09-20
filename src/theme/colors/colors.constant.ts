df8c9952840fdc2162def4a9042e231670c12b00
sarkisovTu <sarkisov1337@gmail.com>
2022-09-20 21:29:56 +0300

    feat: colors theme constants

---
+++ src/theme/colors/colors.constant.ts
@@ -0,0 +1,13 @@
+export const BACKGROUND_COLOR = '#181E1E';
+
+export const COLOR_MINT_100 = '#DDF3EC';
+export const COLOR_MINT_200 = '#CFFFF1';
+export const COLOR_MINT_300 = '#A4F8DE';
+export const COLOR_MINT_400G = '#84D0B9';
+export const COLOR_MINT_600 = '#324F46';
+export const COLOR_MINT_700 = '#283732';
+export const COLOR_MINT_800 = '#1B2121';
+
+export const COLOR_RED_ERROR = '#FF511B';
+
+export const COLOR_MINT_OVERLAY = 'rgba(24, 30, 30, 0.8)';
