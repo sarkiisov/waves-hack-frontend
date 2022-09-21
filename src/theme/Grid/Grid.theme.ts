ae68e6430e9f68de66e2d35139fea6e90004dd64
sarkisovTu <sarkisov1337@gmail.com>
2022-09-21 21:24:25 +0300

    feat: MUI breakpoints interface

---
+++ src/theme/Grid/Grid.theme.ts
@@ -0,0 +1,8 @@
+import { BreakpointsOptions } from '@material-ui/core/styles/createBreakpoints';
+
+export const breakpointOptions: BreakpointsOptions = {
+  keys: ['xs', 'sm', 'md', 'lg', 'xl'],
+  values: {
+    xs: 320, sm: 600, md: 800, lg: 1153, xl: 1680,
+  },
+};
