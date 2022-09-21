935dda5db8d1d8c74e51d12160a50b284d0fc61d
sarkisovTu <sarkisov1337@gmail.com>
2022-09-21 21:21:08 +0300

    feat: Indigo colors scheme

--- src/theme/colors/colors.constant.ts
+++ src/theme/colors/colors.constant.ts
@@ -1,13 +1,20 @@
-export const BACKGROUND_COLOR = '#181E1E';
+export const COLOR_BLACK = '#0000000';
 
-export const COLOR_MINT_100 = '#DDF3EC';
-export const COLOR_MINT_200 = '#CFFFF1';
-export const COLOR_MINT_300 = '#A4F8DE';
-export const COLOR_MINT_400G = '#84D0B9';
-export const COLOR_MINT_600 = '#324F46';
-export const COLOR_MINT_700 = '#283732';
-export const COLOR_MINT_800 = '#1B2121';
+export const BACKGROUND_COLOR = '#0F1010';
+
+export const COLOR_WHITE_SEMI_25 = 'rgba(0, 0, 0, 0.25)';
+
+export const COLOR_TRANSPARENT_100 = 'rgba(27, 28, 33, 0.55)';
+export const COLOR_TRANSPARENT_200 = 'rgba(27, 28, 33, 0.75)';
+
+export const COLOR_INDIGO_100 = '#DDE8F3';
+export const COLOR_INDIGO_200 = '#CFE8FF';
+export const COLOR_INDIGO_300 = '#A4CFF8';
+export const COLOR_INDIGO_400G = '#84ABD0';
+export const COLOR_INDIGO_600 = '#32414F';
+export const COLOR_INDIGO_700 = '#283037';
+export const COLOR_INDIGO_800 = '#1B1E21';
 
 export const COLOR_RED_ERROR = '#FF511B';
 
-export const COLOR_MINT_OVERLAY = 'rgba(24, 30, 30, 0.8)';
+export const COLOR_INDIGO_OVERLAY = 'rgba(24, 30, 30, 0.8)';
