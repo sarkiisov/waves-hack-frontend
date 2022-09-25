3338c44f9a4aba8318776998cbbb17d20dc79146
sarkisovTu <sarkisov1337@gmail.com>
2022-09-25 13:28:45 +0300

    fix: api url

--- src/api/getNativeBalance.ts
+++ src/api/getNativeBalance.ts
@@ -1,8 +1,7 @@
-import path from 'path';
 import { nodeUrl } from './../config/sdk';
 
 export const getNativeBalance = async (userAddress: string) => {
-  const url = new URL(path.join(nodeUrl, `/addresses/balance/${userAddress}`)).toString();
+  const url = nodeUrl.concat(`addresses/balance/${userAddress}`);
   const { balance } = await fetch(url).then(response => response.json());
   return balance;
 }
--- src/api/getTokenBalance.ts
+++ src/api/getTokenBalance.ts
@@ -1,8 +1,7 @@
-import path from 'path';
 import { nodeUrl } from './../config/sdk';
 
 export const getTokenBalance = async (userAddress: string, assetId: string) => {
-  const url = new URL(path.join(nodeUrl, `/assets/balance/${userAddress}/${assetId}`)).toString();
+  const url = nodeUrl.concat(`assets/balance/${userAddress}/${assetId}`);
   const { balance } = await fetch(url).then(response => response.json());
   return balance;
 }
